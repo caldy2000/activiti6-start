e73f9a66a4e18f5b0cce6a0a8852d7d2c9d12759
caldymmm <caldymmm@163.com>
2024-07-15 02:07:17 +0800

    Activiti 与Spring

---
+++ src/test/java/act6/ActivitiInSpringTest.java
@@ -0,0 +1,32 @@
+package act6;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.spring.ProcessEngineFactoryBean;
+import org.activiti.spring.SpringProcessEngineConfiguration;
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@Configuration
+public class ActivitiInSpringTest {
+
+	/**
+	 * 待配置
+	 * @return
+	 */
+	@Bean
+	public SpringProcessEngineConfiguration processEngineConfiguration() {
+		SpringProcessEngineConfiguration configuration = new SpringProcessEngineConfiguration();
+		return configuration;
+	}
+	@Bean
+	public ProcessEngine processEngine() {
+		ProcessEngineFactoryBean processEngineFactoryBean = new ProcessEngineFactoryBean();
+		processEngineFactoryBean.setProcessEngineConfiguration(processEngineConfiguration());
+		return null;
+	}
+}
